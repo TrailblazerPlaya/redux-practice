aaa3b202f28bb78e86696702b53031e677edf9c6
fornara <fornarapro@gmail.com>
2023-11-06 15:38:20 +0300

    Book Library App start

--- redux/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const ADD_CURRENT_TIME = "ADD_CURRENT_TIME";
-export const CLEAR_ALL_TIMES = "CLEAR_ALL_TIMES";
--- redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from 'https://cdn.jsdelivr.net/npm/@reduxjs/toolkit@1.9.5/+esm';
-import reducer from './reducer.js';
-
-export default configureStore({
-    reducer: reducer
-})
